03f056e062fd1447de2cc546e86cacef61a8fddf
Lucas Dietrich <l.dietrich@shopware.com>
2021-09-05 20:21:08 +0200

    Progress on Proximity Detector

--- node-projects/proximity-detector/src/script.js
+++ node-projects/proximity-detector/src/script.js
@@ -20,7 +20,7 @@ const settings = {
         mouseX: 0,
         mouseY: 0
     },
-    elements: {
+    cubes: {
         countX: 14,
         countY: 14,
         width: 1,
@@ -28,10 +28,18 @@ const settings = {
         gap: 2,
         animationDuration: 1,
     },
+    spheres: {
+        countX: 50,
+        countY: 50,
+        radius: 0.125,
+        gap: 3,
+        animationDuration: 0.125,
+    },
     proximity: {
         radius: 6,
         scale: 1,
-        visible: true
+        visible: false,
+        wireframe: false
     }
 }
 
@@ -39,6 +47,14 @@ const settings = {
 // Meshs
 let cubes = [];
 let cubeSettings = [];
+let spheres = [];
+let sphereSettings = {
+    scale: 1,
+    passiveColor: 0x223344,
+    activeColor3: 0x223388,
+    activeColor2: 0x223FBB,
+    activeColor: 0x2244EE,
+}
 let proximityDetector;
 let group;
 
@@ -72,9 +88,9 @@ function init() {
     // INIT Camera
     // --------------------------------------
 
-    camera = new THREE.PerspectiveCamera(75, settings.window.width / settings.window.height, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(45, settings.window.width / settings.window.height, 0.1, 1000);
     // camera = new THREE.OrthographicCamera(-6, 6, -6, 6, 0.1, 100);
-    camera.position.set(0, 0, 10);
+    camera.position.set(0, 0, 30);
     scene.add(camera);
 
 
@@ -104,12 +120,15 @@ function init() {
     addLights();
 
     // Cubes
-    addCubes();
+    // addCubes();
+
+    // Spheres
+    addSpheres();
+
+    console.log(spheres)
 
     // Proximity Detector
     addProximityDetector();
-
-    console.log(cubes)
 }
 
 // Rendering the scene
@@ -118,41 +137,78 @@ function sceneAnimation() {
 
     // Update sensor
     positionOnMouseWorldPosition(proximityDetector);
+
     proximityDetector.scale.set(
       settings.proximity.scale,
       settings.proximity.scale,
       settings.proximity.scale
     );
 
+    proximityDetector.material.visible = settings.proximity.visible;
+    proximityDetector.material.wireframe = settings.proximity.wireframe;
+    proximityDetector.material.needsUpdate = true;
+
     proximityDetector.geometry.attributes.position.needsUpdate = true;
     proximityDetector.scale.needsUpdate = true;
 
-    console.log(t)
+    // console.log(t)
 
     // Update cubes
-    for (let i = 0; i < cubes.length; i++) {
-        const cube = cubes[i];
-        let v = new THREE.Vector3();
-        let p = cube.getWorldPosition(v);
+    /*
+    if (cubes.length > 1) {
+        for (let i = 0; i < cubes.length; i++) {
+            const cube = cubes[i];
+            let v = new THREE.Vector3();
+            let p = cube.getWorldPosition(v);
 
 
-        // Approach 0 to 1 with ease-in-out function in y frames
-        // let x = (t - snapshotFrame) / settings.elements.animationDuration;
-        // let progress = easeInOut(x);
+            // Approach 0 to 1 with ease-in-out function in y frames
+            // let x = (t - snapshotFrame) / settings.cubes.animationDuration;
+            // let progress = easeInOut(x);
 
-        if (p.distanceTo(proximityDetector.position) <= (settings.proximity.radius * settings.proximity.scale)) {
-            cube.scale.set(0.8, 0.8, 0.8);
-            cube.lookAt(proximityDetector.position);
-            cube.material.color.set(0xFF0000);
-        } else {
-            cube.scale.set(1, 1, 1);
-            cube.rotation.set(0, 0, 0);
-            cube.material.color.set(0xFFC107);
+            if (p.distanceTo(proximityDetector.position) <= (settings.proximity.radius * settings.proximity.scale)) {
+                cube.scale.set(0.8, 0.8, 0.8);
+                cube.lookAt(proximityDetector.position);
+                cube.material.color.set(0xFF0000);
+            } else {
+                cube.scale.set(1, 1, 1);
+                cube.rotation.set(0, 0, 0);
+                cube.material.color.set(0xFFC107);
+            }
+
+            cube.geometry.attributes.position.needsUpdate = true;
         }
-
-        cube.geometry.attributes.position.needsUpdate = true;
     }
+     */
 
+    // Update Spheres
+    if (spheres.length > 1) {
+
+        for (let i = 0; i < spheres.length; i++) {
+            const sphere = spheres[i];
+            let v = new THREE.Vector3();
+            let p = sphere.getWorldPosition(v);
+
+
+            // Approach 0 to 1 with ease-in-out function in y frames
+            // let x = (t - snapshotFrame) / settings.sphere.animationDuration;
+            // let progress = easeInOut(x);
+
+            if (p.distanceTo(proximityDetector.position) <= (settings.proximity.radius * settings.proximity.scale)) {
+                sphere.material.color.set(sphereSettings.activeColor);
+                sphere.scale.set(
+                  sphereSettings.scale,
+                  sphereSettings.scale,
+                  sphereSettings.scale
+                );
+            } else {
+                sphere.scale.set(1, 1, 1);
+                sphere.material.color.set(sphereSettings.passiveColor);
+            }
+
+            sphere.geometry.attributes.position.needsUpdate = true;
+        }
+    }
 
     // Update controls
     controls.update();
@@ -175,14 +231,14 @@ function addCubes() {
 
     let i = 0, ix, iy;
 
-    for (ix = 0; ix < settings.elements.countX; ++ix) {
-        for (iy = 0; iy < settings.elements.countY; ++iy) {
+    for (ix = 0; ix < settings.cubes.countX; ++ix) {
+        for (iy = 0; iy < settings.cubes.countY; ++iy) {
 
             // Geometry
             geometryCube = new THREE.BoxGeometry(
-                settings.elements.width,
-                settings.elements.height,
-                settings.elements.height
+                settings.cubes.width,
+                settings.cubes.height,
+                settings.cubes.height
             );
 
             // Material
@@ -191,8 +247,8 @@ function addCubes() {
 
             // Mesh
             meshCube = cubes[i++] = new THREE.Mesh(geometryCube, materialCube);
-            meshCube.position.x = (ix + settings.elements.gap) * 2;
-            meshCube.position.y = (iy + settings.elements.gap) * 2;
+            meshCube.position.x = (ix + settings.cubes.gap) * 2;
+            meshCube.position.y = (iy + settings.cubes.gap) * 2;
             meshCube.position.z = 0;
             group.add(meshCube);
         }
@@ -200,38 +256,91 @@ function addCubes() {
 
     // Group
     group.position.set(
-      settings.elements.countX * (settings.elements.width + settings.elements.gap) * -0.5,
-      settings.elements.countY * (settings.elements.height + settings.elements.gap) * -0.5,
+      settings.cubes.countX * (settings.cubes.width + settings.cubes.gap) * -0.5,
+      settings.cubes.countY * (settings.cubes.height + settings.cubes.gap) * -0.5,
       0
     )
 
     scene.add(group);
 }
 
-
-function addProximityDetector() {
+function addSpheres() {
     let geometrySphere,
         materialSphere,
         meshSphere;
 
+    group = new THREE.Group();
+
     // Geometry
-    geometrySphere = new THREE.SphereGeometry(settings.proximity.radius, 16, 16);
+    geometrySphere = new THREE.SphereBufferGeometry(
+      settings.spheres.radius, 8, 8
+    );
+
+    let i = 0, ix, iy;
+
+    for (ix = 0; ix < settings.spheres.countX; ++ix) {
+        for (iy = 0; iy < settings.spheres.countY; ++iy) {
+
+            // Material
+            materialSphere = new THREE.MeshLambertMaterial({ color: sphereSettings.passiveColor });
+            // materialSphere = new THREE.MeshMatcapMaterial();
+
+            // Mesh
+            meshSphere = spheres[i++] = new THREE.Mesh(geometrySphere, materialSphere);
+            meshSphere.position.x = (ix + settings.spheres.gap);
+            meshSphere.position.y = (iy + settings.spheres.gap);
+            meshSphere.position.z = 0;
+            group.add(meshSphere);
+
+            // GUI
+            // const spheresFolder = gui.addFolder('Spheres');
+            // gui.add(sphereSettings, 'scale').min(0).max(2).step(0.01).name('Radius');
+            // spheresFolder.addColor(sphereSettings, 'passiveColor').name('Inactive Color');
+            // spheresFolder.addColor(sphereSettings, 'activeColor').name('Active Color');
+        }
+    }
+
+    gui.add(sphereSettings, 'scale').min(0).max(2).step(0.01).name('Radius');
+
+    // Group
+    group.position.set(
+      settings.spheres.countX * -0.55,
+      settings.spheres.countY * -0.55,
+      0
+    )
+
+    console.log(group.position);
+
+    scene.add(group);
+}
+
+
+function addProximityDetector() {
+    let geometrySensor,
+        materialSensor,
+        meshSensor;
+
+    // Geometry
+    geometrySensor = new THREE.SphereGeometry(settings.proximity.radius, 16, 16);
 
     // Material
-    materialSphere = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
-    materialSphere.transparent = true;
-    materialSphere.opacity = 0.5;
+    materialSensor = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
+    materialSensor.transparent = true;
+    materialSensor.opacity = 0.5;
+    materialSensor.visible = settings.proximity.visible;
+    materialSensor.wireframe = settings.proximity.wireframe;
 
     // Mesh
-    meshSphere = proximityDetector = new THREE.Mesh(geometrySphere, materialSphere);
-    meshSphere.visible = settings.proximity.visible;
+    meshSensor = proximityDetector = new THREE.Mesh(geometrySensor, materialSensor);
 
-    scene.add(meshSphere);
+    scene.add(meshSensor);
 
     // GUI
-    console.log(meshSphere)
+    console.log(meshSensor)
+    // const sensorFolder = gui.addFolder('Sensor');
     gui.add(settings.proximity, 'scale').min(0).max(4).step(0.01).name('Proximity');
-    gui.add(meshSphere.material, 'visible').name('Visibility');
+    gui.add(settings.proximity, 'visible').name('Visibility');
+    gui.add(settings.proximity, 'wireframe').name('Wireframe');
 }
 
 
@@ -259,18 +368,18 @@ function addLights() {
     const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
     scene.add(ambientLight);
 
-    const pointLight1 = new THREE.PointLight(0xFFEB3B, 1, 500);
-    pointLight1.position.set(8, 5, 8);
+    const pointLight1 = new THREE.PointLight(0x4499FF, 1, 150);
+    pointLight1.position.set(15, 10, 10);
     scene.add(pointLight1);
 
-    const pointLight2 = new THREE.PointLight(0xF39914, 1, 500);
-    pointLight2.position.set(-5, -10, 2);
+    const pointLight2 = new THREE.PointLight(0x22EE99, 1, 150);
+    pointLight2.position.set(-15, -10, 10);
     scene.add(pointLight2);
 
-    const sphereSize = 1;
-    const pointLightHelper1 = new THREE.PointLightHelper( pointLight1, sphereSize );
-    const pointLightHelper2 = new THREE.PointLightHelper( pointLight2, sphereSize );
-    scene.add(pointLightHelper1, pointLightHelper2);
+    // const sphereSize = 1;
+    // const pointLightHelper1 = new THREE.PointLightHelper( pointLight1, sphereSize );
+    // const pointLightHelper2 = new THREE.PointLightHelper( pointLight2, sphereSize );
+    // scene.add(pointLightHelper1, pointLightHelper2);
 }
 
 
